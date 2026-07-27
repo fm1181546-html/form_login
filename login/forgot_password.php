fcfcb56e1d666ca87f5af093cb9eacfd8e3cf226
Fadel <fm1181546@gmail.com>
2026-07-27 07:44:21 +0700

    Update forgot_password.php

--- login/forgot_password.php
+++ login/forgot_password.php
@@ -1,17 +1,11 @@
 <?php
 include 'config.php';
-
 if (isset($_POST['email'])) {
     $email       = $_POST['email'];
     $new_password = password_hash($_POST['new_password'], PASSWORD_DEFAULT);
-
-    // Cek apakah email ada di database
     $cek_email = mysqli_query($conn, "SELECT * FROM user WHERE email = '$email'");
-
     if (mysqli_num_rows($cek_email) > 0) {
-        // Update password baru
         $update = mysqli_query($conn, "UPDATE user SET password = '$new_password' WHERE email = '$email'");
-        
         if ($update) {
             echo "<script>alert('Password berhasil diperbarui!'); window.location='login.html';</script>";
         }
