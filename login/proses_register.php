d102d6946148dda3610e7c18fa8dc988f9e99b22
Fadel <fm1181546@gmail.com>
2026-07-27 07:46:45 +0700

    Update proses_register.php

--- login/proses_register.php
+++ login/proses_register.php
@@ -4,11 +4,9 @@ include 'config.php';
 if (isset($_POST['username'])) {
     $username = $_POST['username'];
     $email    = $_POST['email'];
-    $password = password_hash($_POST['password'], PASSWORD_DEFAULT); // Enkripsi password
-
-    // Cek apakah username sudah ada
-    $cek_user = mysqli_query($conn, "SELECT * FROM user WHERE username = '$username'");
+    $password = password_hash($_POST['password'], PASSWORD_DEFAULT);
     
+    $cek_user = mysqli_query($conn, "SELECT * FROM user WHERE username = '$username'");
     if (mysqli_num_rows($cek_user) > 0) {
         echo "<script>alert('Username sudah terdaftar!'); window.location='register.html';</script>";
     } else {
